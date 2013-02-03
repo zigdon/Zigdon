56b85b733916702af972637b7d0b961851557b61
Dan Boger <dan@peeron.com>
2013-02-02 16:11:06 -0800

    Minor formatting fixes

--- irssi/nsfw.pl
+++ irssi/nsfw.pl
@@ -18,12 +18,12 @@ sub check_enter {
     my ($key) = @_;
     return unless $key eq ENTER;
 
-    my $text    = Irssi::parse_special '$L', undef, 0;
-    my $channel = Irssi::parse_special '$C', undef, 0;
+    my $text    = Irssi::parse_special('$L', undef, 0);
+    my $channel = Irssi::parse_special('$C', undef, 0);
     if (
         (
                Irssi::settings_get_str('nsfw_channels') eq '*'
-            or Irssi::settings_get_str('nsfw_channels') =~ /\Q$channel/i
+            or ($channel and Irssi::settings_get_str('nsfw_channels') =~ /\Q$channel/i)
         )    # channel is in the list
         and $text =~ m#https?://#i    # input matches http://
         and $text !~ /\bn?sfw\b/i     # input doesn't match N?SFW
