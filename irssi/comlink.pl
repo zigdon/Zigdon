87143ad6b4495b4cbe30fd0f17cc5f800cf7e016
Dan Boger <dan@peeron.com>
2013-03-12 15:00:37 -0700

    Remove debugging code

--- irssi/comlink.pl
+++ irssi/comlink.pl
@@ -17,19 +17,13 @@ $VERSION = "0.1";
 use Irssi;
 use Irssi::Irc;
 
-my $once = 0;
 sub cleanup {
   my ($server, $msg, $nick, $address, $target) = @_;
 
-  unless ($once++) {
-    use Data::Dumper;
-    print Dumper \@_;
-  }
 
   if ($nick eq 'comlink') {
     $msg =~ s/^<(\w+)>//;
     if ($1) {
-      Irssi::print("changed -> $1\n");
       $nick = "<$1>";
       Irssi::signal_emit("message public", $server, $msg, $nick, $address, $target);
       Irssi::signal_stop();
