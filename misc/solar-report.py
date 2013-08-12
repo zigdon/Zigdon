f319d8d6156de4ca0563f1c31862b81f03b5b7a8
Dan Boger <dan@peeron.com>
2013-08-12 15:20:41 -0700

    Use proper argument parsing, allow overriding the marker for the bar graph

--- misc/solar-report.py
+++ misc/solar-report.py
@@ -1,20 +1,26 @@
 #!misc/bin/python
 
+import argparse
 import datetime
 import json
 import mechanize
 import sys
 
-DEBUG = False
+parser = argparse.ArgumentParser(description='Load solar generation report')
+parser.add_argument('--debug', '-n', action='store_true')
+parser.add_argument('--marker', default='*')
+parser.add_argument('--date')
+parser.add_argument('rc')
+
+args = parser.parse_args()
 
 # load yesterday's report by default
-date = datetime.date.fromordinal(datetime.date.today().toordinal()-1)
-if len(sys.argv) >= 3:
-    date = sys.argv[2]
+if not args.date:
+    args.date = datetime.date.fromordinal(datetime.date.today().toordinal()-1)
 
-if not DEBUG:
+if not args.debug:
 # load creds
-    with open(sys.argv[1]) as f:
+    with open(args.rc) as f:
         username = f.next().strip()
         password = f.next().strip()
         f.close()
@@ -37,24 +43,24 @@ if not DEBUG:
 # download xls
     br.open(
         'https://oursungevity.com/performance/chart_control/0004A3A18F83/day/%s'
-        % date)
+        % args.date)
     usage = json.loads(br.response().read())
 else:
     usage = json.loads(
         '{"data":[0,0,0,0,0,0,0,0,0.2,0.4,0.8,1.6,3.1,3.5,3.3,3,2.4,1.2,0.4,0.1,0,0,0,0,0]}'
     )
 
-def bar(data, peak=100, width=40):
-    return '*' * int(1.0 * data / peak * width)
+def bar(data, peak=100, width=40, marker='*'):
+    return marker * int(1.0 * data / peak * width)
 
-print 'Solar report for %s' % date
+print 'Solar report for %s' % args.date
 print '=' * 47
 print ''
 
 peak = max(usage['data'])
 for hour in range(24):
     kwh = usage['data'][hour]
-    print '%2d %-40s %.1f' % (hour, bar(kwh, peak=peak), kwh)
+    print '%2d %-40s %.1f' % (hour, bar(kwh, peak=peak, marker=args.marker), kwh)
     hour += 1
 
 print '\nTotal production: %.1f kWh' % sum(usage['data'])
