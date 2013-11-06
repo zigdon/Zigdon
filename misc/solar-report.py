aeb5d886426045917c655da08f2162678e305c09
Dan Boger <dan@peeron.com>
2013-11-06 08:54:38 -0800

    Update for new sungevity site

--- misc/solar-report.py
+++ misc/solar-report.py
@@ -30,25 +30,27 @@ if not args.debug:
     br.set_handle_robots(False)
 
 # load login page
-    br.open('https://oursungevity.com/users/login')
+    br.open('https://star.oursungevity.com/login')
 
 # enter login details
     br.form = list(br.forms())[0]
-    br['data[User][email]'] = username
-    br['data[User][pass]'] = password
+    br['email'] = username
+    br['password'] = password
 
 # log in
     resp = br.submit()
 
 # download xls
     br.open(
-        'https://oursungevity.com/performance/chart_control/0004A3A18F83/day/%s'
+        'https://star.oursungevity.com/api/installation/a1UU0000000JzdRMAS/performance/daily/%s'
         % args.date)
     usage = json.loads(br.response().read())
 else:
     usage = json.loads(
-        '{"data":[0,0,0,0,0,0,0,0,0.2,0.4,0.8,1.6,3.1,3.5,3.3,3,2.4,1.2,0.4,0.1,0,0,0,0,0]}'
+        '[{"date":"2013-11-05 00:00:00","kwh":0.0},{"date":"2013-11-05 01:00:00","kwh":0.0},{"date":"2013-11-05 02:00:00","kwh":0.0},{"date":"2013-11-05 03:00:00","kwh":0.0},{"date":"2013-11-05 04:00:00","kwh":0.0},{"date":"2013-11-05 05:00:00","kwh":0.0},{"date":"2013-11-05 06:00:00","kwh":0.0},{"date":"2013-11-05 07:00:00","kwh":0.128},{"date":"2013-11-05 08:00:00","kwh":1.228},{"date":"2013-11-05 09:00:00","kwh":2.062},{"date":"2013-11-05 10:00:00","kwh":2.438},{"date":"2013-11-05 11:00:00","kwh":2.58},{"date":"2013-11-05 12:00:00","kwh":2.531},{"date":"2013-11-05 13:00:00","kwh":2.179},{"date":"2013-11-05 14:00:00","kwh":1.832},{"date":"2013-11-05 15:00:00","kwh":0.948},{"date":"2013-11-05 16:00:00","kwh":0.088},{"date":"2013-11-05 17:00:00","kwh":-0.009},{"date":"2013-11-05 18:00:00","kwh":0.0},{"date":"2013-11-05 19:00:00","kwh":0.0},{"date":"2013-11-05 20:00:00","kwh":0.0},{"date":"2013-11-05 21:00:00","kwh":0.0},{"date":"2013-11-05 22:00:00","kwh":0.0},{"date":"2013-11-05 23:00:00","kwh":0.0}]'
     )
+
+usage = [ x['kwh'] for x in usage ]
 
 def bar(data, peak=100, width=40, marker='*'):
     return marker * int(1.0 * data / peak * width)
@@ -57,12 +59,12 @@ print 'Solar report for %s' % args.date
 print '=' * 47
 print ''
 
-peak = max(usage['data'])
+peak = max(usage)
 for hour in range(24):
-    kwh = usage['data'][hour]
+    kwh = usage[hour]
     print '%2d %-40s %.1f' % (hour, bar(kwh, peak=peak, marker=args.marker), kwh)
     hour += 1
 
-print '\nTotal production: %.1f kWh' % sum(usage['data'])
+print '\nTotal production: %.1f kWh' % sum(usage)
 
 # vim: ts=4 sw=4 sts=4 :
