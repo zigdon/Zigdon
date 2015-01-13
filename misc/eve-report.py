13a3edc678580d1bf8f4bc76d10fb4f035ecf5be
Dan Boger <dan@peeron.com>
2015-01-13 13:10:09 -0800

    Support multiple api keys, fix indent

--- misc/eve-report.py
+++ misc/eve-report.py
@@ -10,102 +10,107 @@ import evelink.cache.shelf
 
 from collections import OrderedDict
 
+accounts = []
 with open('/home/zigdon/.everc') as f:
-    char_id = int(f.next())
-    key_id = int(f.next())
-    vcode = f.next().strip()
-    keywords = f.next().lower().strip().split(',')
+    for line in f:
+        char_id, key_id, vcode, keywords = line.split(',', 3)
+        char_id = int(char_id)
+        key_id = int(key_id)
+        vcode = vcode.strip()
+        keywords = keywords.lower().strip().split(',')
+        accounts.append((char_id, key_id, vcode, keywords))
     f.close()
 
-api = evelink.api.API(api_key=(key_id, vcode),
-                      cache=evelink.cache.shelf.ShelveCache('/tmp/evecache-wallet'))
-char = evelink.char.Char(char_id=char_id, api=api)
-
-journal, _, _ = char.wallet_journal(limit=500)
-
-categories = OrderedDict()
 def default_entry():
-  return {'bounties': 0, 'duty': 0, 'sales': 0, 'purchases': 0}
+    return {'bounties': 0, 'duty': 0, 'sales': 0, 'purchases': 0}
 
-details = ''
-for entry in journal:
-  date = humanize.naturalday(datetime.datetime.fromtimestamp(entry['timestamp']))
-  if entry['amount'] >= 0:
-    party = entry['party_1']['name']
-  else:
-    party = entry['party_2']['name']
-  reason = entry['reason']
-  if party == 'CONCORD' and re.match('\d+:\d', reason):
-    reason = 'Bounty'
-    if date not in categories:
-        categories[date] = default_entry()
+for char_id, key_id, vcode, keywords in accounts:
 
-    categories[date]['bounties'] += entry['amount']
+    api = evelink.api.API(api_key=(key_id, vcode),
+                          cache=evelink.cache.shelf.ShelveCache('/tmp/evecache-wallet'))
+    char = evelink.char.Char(char_id=char_id, api=api)
 
-  if re.match('(import|export) duty', reason.lower()):
-    if date not in categories:
-        categories[date] = default_entry()
-    categories[date]['duty'] += entry['amount']
+    journal, _, _ = char.wallet_journal(limit=500)
+    categories = OrderedDict()
+    details = ''
+    for entry in journal:
+        date = humanize.naturalday(datetime.datetime.fromtimestamp(entry['timestamp']))
+        if entry['amount'] >= 0:
+            party = entry['party_1']['name']
+        else:
+            party = entry['party_2']['name']
+        reason = entry['reason']
+        if party == 'CONCORD' and re.match('\d+:\d', reason):
+            reason = 'Bounty'
+            if date not in categories:
+                categories[date] = default_entry()
 
-  details += '%10s | %26s | %13s | %16s | %s\n' % \
-    (date, party, humanize.intcomma(entry['amount']),
-    humanize.intcomma(entry['balance']), reason)
+            categories[date]['bounties'] += entry['amount']
 
-transactions, _, _ = char.wallet_transactions()
-for entry in transactions:
-  date = humanize.naturalday(datetime.datetime.fromtimestamp(entry['timestamp']))
-  if date not in categories:
-    continue
+        if re.match('(import|export) duty', reason.lower()):
+            if date not in categories:
+                categories[date] = default_entry()
+            categories[date]['duty'] += entry['amount']
 
-  if entry['action'] == 'buy':
-    categories[date]['purchases'] += entry['price'] * entry['quantity']
-  elif entry['action'] == 'sell':
-    categories[date]['sales'] += entry['price'] * entry['quantity']
+        details += '%10s | %26s | %13s | %16s | %s\n' % \
+          (date, party, humanize.intcomma(entry['amount']),
+          humanize.intcomma(entry['balance']), reason)
 
-print '\n%10s  %12s  %12s  %12s  %12s' % (
-    'Summary', 'Bounties', 'Duty', 'Sales', 'Purchases')
-for date, cats in categories.iteritems():
-    print '%10s: %12s  %12s  %12s  %12s' % (
-      date, humanize.intcomma(int(cats['bounties'])),
-      humanize.intcomma(int(cats['duty'])),
-      humanize.intcomma(int(cats['sales'])),
-      humanize.intcomma(int(cats['purchases'])),
-      )
+    transactions, _, _ = char.wallet_transactions()
+    for entry in transactions:
+        date = humanize.naturalday(datetime.datetime.fromtimestamp(entry['timestamp']))
+        if date not in categories:
+            continue
 
-print '\nBalance: %s' % humanize.intcomma(int(journal[-1]['balance']))
+        if entry['action'] == 'buy':
+            categories[date]['purchases'] += entry['price'] * entry['quantity']
+        elif entry['action'] == 'sell':
+            categories[date]['sales'] += entry['price'] * entry['quantity']
 
-print '\nUpcoming events of note:'
-events = char.calendar_events()
-for eid, event in events[0].iteritems():
-    for keyword in keywords:
-        if keyword in event['title'].lower():
-            start = humanize.naturalday(datetime.datetime.fromtimestamp(event['start_ts']))
+    print '\n%10s  %12s  %12s  %12s  %12s' % (
+        'Summary', 'Bounties', 'Duty', 'Sales', 'Purchases')
+    for date, cats in categories.iteritems():
+        print '%10s: %12s  %12s  %12s  %12s' % (
+          date, humanize.intcomma(int(cats['bounties'])),
+          humanize.intcomma(int(cats['duty'])),
+          humanize.intcomma(int(cats['sales'])),
+          humanize.intcomma(int(cats['purchases'])),
+          )
 
-            print '%s - %s\n' % (start, event['title'])
+    print '\nBalance: %s' % humanize.intcomma(int(journal[-1]['balance']))
 
-planets, _, _ = char.planetary_colonies()
-alerts = OrderedDict()
-for id, planet in planets.items():
-    name = planet['planet']['name']
+    print '\nUpcoming events of note:'
+    events = char.calendar_events()
+    for eid, event in events[0].iteritems():
+        for keyword in keywords:
+            if keyword in event['title'].lower():
+                start = humanize.naturalday(datetime.datetime.fromtimestamp(event['start_ts']))
 
-    alerts[name] = []
-    pins, _, _ = char.planetary_pins(id)
-    now = datetime.datetime.now()
-    for pin in pins.values():
-        pintype = pin['type']['name']
-        if re.search('Extractor', pintype):
-            end = datetime.datetime.fromtimestamp(pin['expiry_ts'])
-            timeleft = end - now
-            if timeleft < datetime.timedelta(1):
-                alerts[name].append('- extractor ends in %s' % timeleft)
+                print '%s - %s\n' % (start, event['title'])
 
-for planet, issues in alerts.items():
-    if not issues:
-        continue
-    print 'PI issues at %s:' % planet
-    for issue in issues:
-        print issue
+    planets, _, _ = char.planetary_colonies()
+    alerts = OrderedDict()
+    for id, planet in planets.items():
+        name = planet['planet']['name']
 
-if False:
-    print 'Transaction details:\n'
-    print details
+        alerts[name] = []
+        pins, _, _ = char.planetary_pins(id)
+        now = datetime.datetime.now()
+        for pin in pins.values():
+            pintype = pin['type']['name']
+            if re.search('Extractor', pintype):
+                end = datetime.datetime.fromtimestamp(pin['expiry_ts'])
+                timeleft = end - now
+                if timeleft < datetime.timedelta(1):
+                    alerts[name].append('- extractor ends in %s' % timeleft)
+
+    for planet, issues in alerts.items():
+        if not issues:
+            continue
+        print 'PI issues at %s:' % planet
+        for issue in issues:
+            print issue
+
+    if False:
+        print 'Transaction details:\n'
+        print details
