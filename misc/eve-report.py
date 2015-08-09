833efd47600180e0cb11dc393483a757b30bdd14
Dan Boger <dan@peeron.com>
2015-08-08 19:55:11 -0700

    Use schematicID to count installation types.

--- misc/eve-report.py
+++ misc/eve-report.py
@@ -10,6 +10,7 @@ import re
 import evelink.cache.sqlite
 
 from collections import OrderedDict, defaultdict
+from pprint import pprint
 
 FLAGS = gflags.FLAGS
 
@@ -66,6 +67,78 @@ ITEMS = {
     9848: {'tier': 3, 'name': 'Robotics', 'type_id': 9848, 'req': [3689, 9836]},
     17392: {'tier': 3, 'name': 'Data Chips', 'type_id': 17392, 'req': [2312, 2327]},
 }
+
+SCHEMATICS = {i: {'id': i, 'name': n, 'cycle': c} for i, n, c in [
+    # schematicID schematicName   cycleTime
+    (65, "Superconductors", 3600),
+    (66, "Coolant", 3600),
+    (67, "Rocket Fuel", 3600),
+    (68, "Synthetic Oil", 3600),
+    (69, "Oxides", 3600),
+    (70, "Silicate Glass", 3600),
+    (71, "Transmitter", 3600),
+    (72, "Water-Cooled CPU", 3600),
+    (73, "Mechanical Parts", 3600),
+    (74, "Construction Blocks", 3600),
+    (75, "Enriched Uranium", 3600),
+    (76, "Consumer Electronics", 3600),
+    (77, "Miniature Electronics", 3600),
+    (78, "Nanites", 3600),
+    (79, "Biocells", 3600),
+    (80, "Microfiber Shielding", 3600),
+    (81, "Viral Agent", 3600),
+    (82, "Fertilizer", 3600),
+    (83, "Genetically Enhanced Livestock", 3600),
+    (84, "Livestock", 3600),
+    (85, "Polytextiles", 3600),
+    (86, "Test Cultures", 3600),
+    (87, "Supertensile Plastics", 3600),
+    (88, "Polyaramids", 3600),
+    (89, "Ukomi Superconductor", 3600),
+    (90, "Condensates", 3600),
+    (91, "Camera Drones", 3600),
+    (92, "Synthetic Synapses", 3600),
+    (94, "High-Tech Transmitter", 3600),
+    (95, "Gel-Matrix Biopaste", 3600),
+    (96, "Supercomputers", 3600),
+    (97, "Robotics", 3600),
+    (98, "Smartfab Units", 3600),
+    (99, "Nuclear Reactors", 3600),
+    (100, "Guidance Systems", 3600),
+    (102, "Neocoms", 3600),
+    (103, "Planetary Vehicles", 3600),
+    (104, "Biotech Research Reports", 3600),
+    (105, "Vaccines", 3600),
+    (106, "Industrial Explosives", 3600),
+    (107, "Hermetic Membranes", 3600),
+    (108, "Transcranial Microcontroller", 3600),
+    (109, "Data Chips", 3600),
+    (110, "Hazmat Detection Systems", 3600),
+    (111, "Cryoprotectant Solution", 3600),
+    (112, "Organic Mortar Applicators", 3600),
+    (113, "Sterile Conduits", 3600),
+    (114, "Nano-Factory", 3600),
+    (115, "Self-Harmonizing Power Core", 3600),
+    (116, "Recursive Computing Module", 3600),
+    (117, "Broadcast Node", 3600),
+    (118, "Integrity Response Drones", 3600),
+    (119, "Wetware Mainframe", 3600),
+    (121, "Water", 1800),
+    (122, "Plasmoids", 1800),
+    (123, "Electrolytes", 1800),
+    (124, "Oxygen", 1800),
+    (125, "Oxidizing Compound", 1800),
+    (126, "Reactive Metals", 1800),
+    (127, "Precious Metals", 1800),
+    (128, "Toxic Metals", 1800),
+    (129, "Chiral Structures", 1800),
+    (130, "Silicon", 1800),
+    (131, "Bacteria", 1800),
+    (132, "Biomass", 1800),
+    (133, "Proteins", 1800),
+    (134, "Biofuels", 1800),
+    (135, "Industrial Fibers", 1800),
+]}
 
 SIZES = (0.01, 0.38, 1.5, 6)
 for v in ITEMS.values():
@@ -166,11 +239,11 @@ def get_planetary_alerts():
             print '  %s:' % section.upper()
             for resource in sorted(data[section],
                                    key=lambda x: (ITEMS[x]['tier'], name(x))):
-                if section == 'has':
-                    name_and_count = '   %s' % name(resource)
+                if section != 'has' and name(resource) in data['counts']:
+                    count = data['counts'][name(resource)]
+                    name_and_count = '%dx %s' % (count, name(resource))
                 else:
-                    name_and_count = '%dx %s' % (data['counts'][resource],
-                                                 name(resource))
+                    name_and_count = '   %s' % name(resource)
                 print '    %-21s %d (%d m3) [%s]' % (
                     name_and_count, data[section][resource],
                     ITEMS[resource]['size'] * data[section][resource],
@@ -228,6 +301,12 @@ def get_planetary_alerts():
         now = datetime.datetime.now()
         for pin in pins.values():
             pin_type = get_pin_type(pin['type']['name'])
+            schematic = pin['schematic']
+
+            if schematic in SCHEMATICS:
+                schematic_name = SCHEMATICS[schematic]['name']
+                planet_state[planet_id]['counts'][schematic_name] += 1
+
             if pin_type == 'Extractor':
                 if pin['expiry_ts'] is not None:
                     end = datetime.datetime.fromtimestamp(pin['expiry_ts'])
@@ -257,7 +336,6 @@ def get_planetary_alerts():
 
                     if pin_type == 'Extractor':
                         planet_state[planet_id]['makes'][type_id] += qty
-                        planet_state[planet_id]['counts'][type_id] += 1
                     elif route['source_id'] == pin['id']:
                         qty *= -1
 
@@ -267,10 +345,8 @@ def get_planetary_alerts():
                     if pin_type in ('Basic', 'Advanced'):
                         if qty < 0:
                             planet_state[planet_id]['makes'][type_id] -= qty
-                            planet_state[planet_id]['counts'][type_id] += 1
                         else:
                             planet_state[planet_id]['needs'][type_id] += qty
-                            planet_state[planet_id]['counts'][type_id] += 1
 
                 # check that we have enough storage for the next day
                 if pin_type in ('Launchpad', 'Storage') and delta > 0:
@@ -294,6 +370,9 @@ def get_planetary_alerts():
         if FLAGS.show_state or FLAGS.debug_planet:
             print "\n\nState for %s:\n" % planet_name
             planet_summary(planet_state[planet_id])
+
+        if FLAGS.debug:
+            pprint(planet_state)
 
         # check that we have what we need for the next day
         alert = None
